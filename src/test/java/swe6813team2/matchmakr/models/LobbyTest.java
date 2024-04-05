35fa8be9506265da4ae6fa1b5287cc10720919db
reedmccallum <14640761+reedmccallum@users.noreply.github.com>
2024-04-05 19:03:49 -0400

    remove inapplicable tests and improve logic of testCreatedBeforeUpdated and set access from public to default
    No need to test that lobby.getId is not null, since ID is supplied by the database.
    
    Rewrote logic for testCreatedBeforeUpdated to allow the two times to be equal and still pass

--- src/test/java/swe6813team2/matchmakr/models/LobbyTest.java
+++ src/test/java/swe6813team2/matchmakr/models/LobbyTest.java
@@ -3,11 +3,9 @@ package swe6813team2.matchmakr.models;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LobbyTest {
+class LobbyTest {
 
     private Lobby lobby;
 
@@ -17,32 +15,27 @@ public class LobbyTest {
     }
 
     @Test
-    public void testIdNotNull() {
-        assertNotNull(lobby.getId());
-    }
-
-    @Test
-    public void testGameDefaultValue() {
+    void testGameDefaultValue() {
         assertEquals(0, lobby.getGame());
     }
 
     @Test
-    public void testMaxPlayersDefaultValue() {
+    void testMaxPlayersDefaultValue() {
         assertEquals(0, lobby.getMaxPlayers());
     }
 
     @Test
-    public void testCreatedNotNull() {
+    void testCreatedNotNull() {
         assertNotNull(lobby.getCreated());
     }
 
     @Test
-    public void testUpdatedNotNull() {
+    void testUpdatedNotNull() {
         assertNotNull(lobby.getUpdated());
     }
 
     @Test
-    public void testCreatedBeforeUpdated() {
-        assertTrue(lobby.getCreated().before(lobby.getUpdated()));
+    void testCreatedBeforeUpdated() {
+        assertTrue(lobby.getCreated().getTime() <= lobby.getUpdated().getTime());
     }
 }
