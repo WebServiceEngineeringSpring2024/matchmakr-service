b82479b24751bfce9f556bb0a8b7345e1ada49be
reedmccallum <14640761+reedmccallum@users.noreply.github.com>
2024-04-05 19:21:41 -0400

    fix camelcase

--- src/main/java/swe6813team2/matchmakr/repositories/UserRepository.java
+++ src/main/java/swe6813team2/matchmakr/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import swe6813team2.matchmakr.models.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByuserName(String username);
+    Optional<User> findByUserName(String username);
 
     Optional<User> findByEmail(String email);//custom method for finding by a specific variable/column name, the name of function must match model variable!
 }
