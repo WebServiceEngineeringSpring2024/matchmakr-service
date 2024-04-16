4e972224b3d1495d78bc385853e4e84fca17884a
RoopleDoops <ryanmatthewsmith13@gmail.com>
2024-04-16 12:16:26 -0400

    Update findMatchingUsers SQL Query to run by user's email
    - Updated findMatchingUsers query in userRepository to match by email passed as a parameter.
    - Updated LobbyController to pass in user email as a parameter when calling the getMatchingUsers function from lobbyService.
    - LobbyService did not require updating, as it was updated in Ralph's last edit to these functions.
    - Removed unnecessary code from LobbyRepository.

--- src/main/java/swe6813team2/matchmakr/repositories/UserRepository.java
+++ src/main/java/swe6813team2/matchmakr/repositories/UserRepository.java
@@ -9,32 +9,25 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByUserName(String username);
+    Optional<User> findByUserName(String username);
 
-	Optional<User> findByEmail(String email);//custom method for finding by a specific variable/column name, the name of function must match model variable!
-
+    Optional<User> findByEmail(String email);//custom method for finding by a specific variable/column name, the name of function must match model variable!
+	
 	@Query(
 			nativeQuery = true,
 			value = "SELECT u.* FROM user u " +
-					"JOIN user me ON me.id = 1004 " +
+					"JOIN user me ON me.email = :uEmail " +
 					"JOIN personality mp ON mp.id = me.personality " +
 					"JOIN personality p ON p.id = u.personality " +
 					"WHERE " +
-					"p.aggression BETWEEN mp.aggression - 2 AND " +
-					"mp.aggression + 2 AND " +
-					"p.kindness BETWEEN mp.kindness - 2 AND " +
-					"mp.kindness + 2 AND " +
-					"p.competitiveness BETWEEN mp.competitiveness - 2 AND " +
-					"mp.competitiveness + 2 AND " +
-					"u.id <> me.id " +
+					"u.online = 1 " +
+					"AND p.aggression BETWEEN mp.aggression - 2 AND mp.aggression + 2 " +
+					"AND p.kindness BETWEEN mp.kindness - 2 AND mp.kindness + 2 " +
+					"AND p.competitiveness BETWEEN mp.competitiveness - 2 AND mp.competitiveness + 2 " +
+					"AND u.id <> me.id " +
 					"ORDER BY (ABS(mp.aggression - p.aggression) + ABS(mp.kindness - p.kindness) + ABS(mp.competitiveness - p.competitiveness)) / 3 " +
-					"LIMIT 3;")
+					"LIMIT 10;")
 	List<User> findMatchingUsers(
-			@Param("aScoreMin") Integer aScoreMin,
-			@Param("aScoreMax") Integer aScoreMax,
-			@Param("kScoreMin") Integer kScoreMin,
-			@Param("kScoreMax") Integer kScoreMax,
-			@Param("cScoreMin") Integer cScoreMin,
-			@Param("cScoreMax") Integer cScoreMax);
+			@Param("uEmail") String userEmail);
 
 }
